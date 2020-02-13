2cc24a85922a6d053f7aa2c284aa07e9ccddedfa
josa <josa@josa-desktop>
2020-02-13 11:07:06 -0300

    metodo simples

---
+++ josue_beta/src/josue_beta/Josue.java
@@ -0,0 +1,16 @@
+package josue_beta;
+
+public class Josue {
+
+	
+	public static int soma(int x, int y) {
+		return x+y;
+	}
+	public static void main(String[] args) {
+		int n1 = 5;
+		int n2 = 7;
+		int resultado = soma(n1,n2);
+		System.out.println(resultado);
+	}
+
+}
